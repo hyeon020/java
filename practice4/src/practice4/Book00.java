c098ff3bcbff49ffc9b2dcd35a93da01d04bfcf7
hyeon020 <105904784+hyeon020@users.noreply.github.com>
2022-12-10 01:17:54 +0900

    first commit

---
+++ practice4/src/practice4/Book00.java
@@ -0,0 +1,29 @@
+package practice4;
+
+public class Book00 {
+	String title;
+	String author;
+	
+	void show() { System.out.println(title + " " + author); }
+	
+	public Book00() {
+		this("", "");
+		System.out.println("생성자 호출됨");
+	}
+	
+	public Book00(String title) {
+		this(title, "작자미상");
+	}
+	
+	public Book00(String title, String author) {
+		this.title = title;
+		this.author = author;
+	}
+	
+	public static void main(String[] args) {
+		Book00 littlePrince = new Book00("어린왕자", "생텍쥐페리");
+		Book00 loveStory = new Book00("춘향전");
+		Book00 emptyBook00 = new Book00();
+		loveStory.show();
+	}
+}
---
+++ practice4/src/practice4/Circle00.java
@@ -0,0 +1,25 @@
+package practice4;
+
+public class Circle00{
+	int radius;
+	String name;
+	
+	public double getArea() {
+		return 3.14*radius*radius;
+	}
+	
+	public static void main(String[] args) {
+		Circle00 pizza;
+		pizza = new Circle00();
+		pizza.radius = 10;
+		pizza.name = "자바피자";
+		double area = pizza.getArea();
+		System.out.println(pizza.name + "의 면적은 " + area);
+		
+		Circle00 donut = new Circle00();
+		donut.radius = 2;
+		donut.name = "자바도넛";
+		area = donut.getArea();
+		System.out.println(donut.name + "의 면적은 " + area);
+	}
+}
