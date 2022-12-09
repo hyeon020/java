e77b132479d620d77d66d04046ef602f23173dcd
hyeon020 <105904784+hyeon020@users.noreply.github.com>
2022-12-10 01:32:45 +0900

    practice4

--- practice4/src/practice4/Circle00.java
+++ practice4/src/practice4/Circle00.java
@@ -14,12 +14,12 @@ public class Circle00{
 		pizza.radius = 10;
 		pizza.name = "자바피자";
 		double area = pizza.getArea();
-		System.out.println(pizza.name + "의 면적은 " + area);
+		System.out.println(pizza.name + "의 면적은 " + area); //피자의 면적
 		
 		Circle00 donut = new Circle00();
 		donut.radius = 2;
 		donut.name = "자바도넛";
 		area = donut.getArea();
-		System.out.println(donut.name + "의 면적은 " + area);
+		System.out.println(donut.name + "의 면적은 " + area); //도넛의 면적
 	}
 }
