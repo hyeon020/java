ca421e45228e9baf0066814a6f591d0c9f4fa18d
hyeon020 <105904784+hyeon020@users.noreply.github.com>
2022-12-10 18:42:31 +0900

    Client

--- practice14/src/practice14/ClientLdh.java
+++ practice14/src/practice14/ClientLdh.java
@@ -10,13 +10,13 @@ public class ClientLdh {
 		Socket socket = null;
 		Scanner scanner = new Scanner(System.in);
 		try {
-			socket = new Socket("localhost", 9999);
+			socket = new Socket("localhost", 9999); //socket생성
 			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 			while(true) {
 				System.out.print("보내기>>");
 				String outputMessage = scanner.nextLine();
-				if(outputMessage.equalsIgnoreCase("bye")) {
+				if(outputMessage.equalsIgnoreCase("bye")) { 
 					out.write(outputMessage+"\n");
 					out.flush();
 					break;
@@ -30,9 +30,9 @@ public class ClientLdh {
 			System.out.println(e.getMessage());
 		}finally {
 			try {
-				scanner.close();
+				scanner.close(); //scanner 종료
 				if(socket!=null) socket.close();
-			} catch(IOException e) {
+			} catch(IOException e) { 
 				System.out.println("서버와 채팅 중 오류가 발생했습니다.");
 			}
 		}
