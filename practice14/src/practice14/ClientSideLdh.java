d8f4c16edf23947ac97d55b7145cfb450e0e6100
hyeon020 <105904784+hyeon020@users.noreply.github.com>
2022-12-10 18:40:35 +0900

    ClientSide

--- practice14/src/practice14/ClientSideLdh.java
+++ practice14/src/practice14/ClientSideLdh.java
@@ -6,8 +6,8 @@ public class ClientSideLdh {
         String server = args[0];
         int port = Integer.parseInt(args[1]);
         Socket c = new Socket(server, port);
-        InputStream is = c.getInputStream();
-        DataInputStream dis = new DataInputStream(is);
+        InputStream is = c.getInputStream();  
+        DataInputStream dis = new DataInputStream(is); //데이터 받기
         for(int i=1 ; i <= 10 ; i++) {
             int j = dis.readInt();
             System.out.println("서버로부터 받은 데이터 " + j + " 출력");
