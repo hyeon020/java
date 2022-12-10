f5ee123a4467faf7966a0c394105deb68e96dc76
hyeon020 <105904784+hyeon020@users.noreply.github.com>
2022-12-10 18:29:35 +0900

    practice4 예제

--- practice4/src/practice4/Book00.java
+++ practice4/src/practice4/Book00.java
@@ -8,11 +8,11 @@ public class Book00 {
 	
 	public Book00() {
 		this("", "");
-		System.out.println("생성자 호출됨");
+		System.out.println("생성자 호출됨"); //생성자 호출 완료 출력
 	}
 	
 	public Book00(String title) {
-		this(title, "작자미상");
+		this(title, "작자미상"); 
 	}
 	
 	public Book00(String title, String author) {
