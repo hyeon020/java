e58916d2437daf479bce963ba58e2a85c29e71c2
hyeon020 <105904784+hyeon020@users.noreply.github.com>
2022-12-10 18:37:32 +0900

    practice14 ServerSide

--- practice14/src/practice14/ServerSideLdh.java
+++ practice14/src/practice14/ServerSideLdh.java
@@ -2,10 +2,10 @@ package practice14;
 import java.io.*;
 import java.net.*;
 
-class ServerSideLdh {
+class ServerSideLdh { //ServerSide클래스
     public static void main(String args[]) throws Exception {
-        int port = Integer.parseInt(args[0]);
-        int times = Integer.parseInt(args[1]);
+        int port = Integer.parseInt(args[0]); //정수형 port
+        int times = Integer.parseInt(args[1]); //정수형 times
         ServerSocket ss = new ServerSocket(port);
         int i = 1;
         while( i <= times) {
