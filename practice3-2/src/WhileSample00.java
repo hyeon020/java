49b08f9f06a32fb3df16c944cc5f347103ce0a55
hyeon020 <105904784+hyeon020@users.noreply.github.com>
2022-12-10 18:36:02 +0900

    practice3-2

--- practice3-2/src/WhileSample00.java
+++ practice3-2/src/WhileSample00.java
@@ -7,8 +7,8 @@ public class WhileSample00 {
 		
 		System.out.println("정수를 입력하고 마지막에 0을 입력하세요.");
 		while((n=scanner.nextInt()) != 0) {
-			sum = sum + n;
-			count++;
+			sum = sum + n;  //합 sum
+			count++;   //수의 개수 1개씩 증가
 		}
 		System.out.print("수의 개수는 " + count + "개이며 ");
 		System.out.println("평균은 " + sum/count + "입니다.");
