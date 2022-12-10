c4aed60f90755f251026b87c21f788eda8888b9e
hyeon020 <105904784+hyeon020@users.noreply.github.com>
2022-12-10 18:32:15 +0900

    NestedLoop

--- practice3/src/NestedLoop00.java
+++ practice3/src/NestedLoop00.java
@@ -3,9 +3,9 @@ public class NestedLoop00 {
 		for(int i=2; i<10; i+=3) {
 			for(int j=1; j<10; j++) {
 				for(int k=i; k<3+i; k++) {
-					if(k>9)
-						break;
-					System.out.print(k + "*" + j + "=" + k*j + "\t");
+					if(k>9) //k가 9보다 클경우
+						break; //빠져나감
+					System.out.print(k + "*" + j + "=" + k*j + "\t"); 
 				}
 				System.out.println();
 			}
