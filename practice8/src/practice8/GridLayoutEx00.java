061869a4678a5041d5a9b599c3b3b1d0b43abcb2
hyeon020 <105904784+hyeon020@users.noreply.github.com>
2022-12-13 21:36:11 +0900

    practice8

--- practice8/src/practice8/GridLayoutEx00.java
+++ practice8/src/practice8/GridLayoutEx00.java
@@ -8,14 +8,14 @@ public class GridLayoutEx00 extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE );
 		Container contentPane = getContentPane();
 		
-		contentPane.setLayout(new GridLayout(1, 10));
+		contentPane.setLayout(new GridLayout(1, 10)); //girdLayout 위치 지정
 		
-		for(int i=0; i<10; i++) {
+		for(int i=0; i<10; i++) { //i는 0부터 9까지
 			String text = Integer.toString(i);
 			JButton button = new JButton(text);
 			contentPane.add(button);
 		}
-		setSize(500, 200);
+		setSize(500, 200); //사이즈 지정
 		setVisible(true);
 	}
 	public static void main(String[] args) {
