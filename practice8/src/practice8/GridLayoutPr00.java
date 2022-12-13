315262e7a7f267dc621b3569ce4ce8ac64eaeb98
hyeon020 <105904784+hyeon020@users.noreply.github.com>
2022-12-13 21:35:17 +0900

    practice8

--- practice8/src/practice8/GridLayoutPr00.java
+++ practice8/src/practice8/GridLayoutPr00.java
@@ -4,15 +4,15 @@ import java.awt.*;
 
 public class GridLayoutPr00 extends JFrame {
 	public GridLayoutPr00() {
-		setTitle("Ten Color Buttons Frame");
+		setTitle("Ten Color Buttons Frame"); //제목 지정
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container contentPane = getContentPane();
 		
-		contentPane.setLayout(new GridLayout(1, 10));
+		contentPane.setLayout(new GridLayout(1, 10)); //GridLayout 위치 지정
 		
 		Color [] color = {Color.RED, Color.ORANGE, Color.YELLOW, Color.GREEN,
 				Color.CYAN, Color.BLUE, Color.MAGENTA, Color.GRAY, Color.PINK,
-				Color.LIGHT_GRAY };
+				Color.LIGHT_GRAY }; //색깔 배열
 		
 		for(int i=0; i<10; i++) {
 			JButton button = new JButton(Integer.toString(i));
