b9efcf5e8b04be5e77ad390d46e53ea00790120d
hyeon020 <105904784+hyeon020@users.noreply.github.com>
2022-12-13 21:34:35 +0900

    practice8

--- practice8/src/practice8/MyFrame.java
+++ practice8/src/practice8/MyFrame.java
@@ -3,12 +3,12 @@ import javax.swing.*;
 
 public class MyFrame extends JFrame {
 	public MyFrame() {
-		setTitle("300x300 스윙 프레임 만들기");
-		setSize(300, 300);
-		setVisible(true);
+		setTitle("300x300 스윙 프레임 만들기"); //스윙프레임 제목 지정
+		setSize(300, 300); //size 지정
+		setVisible(true); 
 	}
 	
 	public static void main(String[] args) {
-		MyFrame frame = new MyFrame();
+		MyFrame frame = new MyFrame(); //new Frame 생성
 	}
 }
